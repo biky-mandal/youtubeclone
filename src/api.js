6dde85f1dace6b5b19ba4e945d0a03855c8b0048
biky-mandal <m.biky50@gmail.com>
2024-02-25 21:34:44 +0530

    update

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const request = axios.create({
     baseURL: 'https://youtube.googleapis.com/youtube/v3/',
     params: {
-        key: 'AIzaSyCU9346Ovzzi6tLjqxedtyQnlB48LvQO-Y'
+        key: `${process.env.API_KEY}`
     }
 })
 
--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCU9346Ovzzi6tLjqxedtyQnlB48LvQO-Y",
+    apiKey: `${process.env.API_KEY}`,
     authDomain: "ytcloneapp.firebaseapp.com",
     projectId: "ytcloneapp",
     storageBucket: "ytcloneapp.appspot.com",
     messagingSenderId: "706751599572",
     appId: "1:706751599572:web:162c894f81cbafb5113c41"
-};
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
